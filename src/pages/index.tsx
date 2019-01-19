69dfc00240841b27960c499b1fd0889266da4664
Christian Rackerseder <git@echooff.de>
2019-01-19 13:51:24 +0100

    👩‍🎤 Coloring "Hello world!" with Emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
 import React from 'react';
+import { css } from '@emotion/core';
 
-export default () => <div>Hello World!</div>;
+const styles = css({
+  color: 'blue',
+});
+
+export default () => <div css={styles}>Hello World!</div>;
