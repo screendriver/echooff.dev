81c3542ac4b3605657d6cfc07040a94f26c7cb05
Christian Rackerseder <git@echooff.de>
2019-01-19 14:00:50 +0100

    ✨ Create <Head /> component

---
+++ src/components/Head.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export function Head() {
+  return (
+    <Helmet>
+      <link rel="shortcut icon" href="favicon.png" />
+    </Helmet>
+  );
+}
