178aef28071fc122db7b71b34951cc37bf504799
Christian Rackerseder <git@echooff.de>
2019-01-19 14:07:31 +0100

    📄 Include <GitHubCorner /> in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import 'modern-normalize';
 import React from 'react';
 import { css } from '@emotion/core';
 import { Head } from '../components/Head';
+import { GitHubCorner } from '../components/GitHubCorner';
 
 const styles = css({
   color: 'blue',
@@ -10,6 +11,7 @@ const styles = css({
 export default () => (
   <>
     <Head />
+    <GitHubCorner />
     <div css={styles}>Hello World!</div>
   </>
 );
