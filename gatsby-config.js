735f7f7daa8e5ac242d66052ead6c3eaf47aa7f2
Christian Rackerseder <git@echooff.de>
2019-01-19 14:17:24 +0100

    ⚙️ Enable gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,12 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 };
