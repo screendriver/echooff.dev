1e2a88549e9d385b560702649fb8437d5a2f1807
Christian Rackerseder <git@echooff.de>
2019-01-22 09:35:41 +0100

    📄 Add more text to <About />

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,12 +7,12 @@ interface AboutProps {
 }
 
 const AboutStyled = styled.div({
+  fontFamily: 'Open sans,sans-serif',
   padding: '100px 0',
   textAlign: 'center',
 });
 
 const Title = styled.h1({
-  fontFamily: 'Open sans,sans-serif',
   fontWeight: 400,
   color: '#121d1f',
 });
@@ -30,12 +30,40 @@ const AboutImage = styled(Img)({
   zIndex: -1,
 });
 
+const Text = styled.p({
+  color: '#656c6d',
+  fontSize: 14,
+  marginTop: 20,
+  marginRight: '20%',
+  marginLeft: '20%',
+  textAlign: 'left',
+});
+
+const GoneText = styled.h5({
+  textTransform: 'uppercase',
+  color: '#656c6d',
+  fontSize: 14,
+  fontWeight: 700,
+});
+
 export function About({ image }: AboutProps) {
   return (
     <AboutStyled id="about">
       <Title>About</Title>
       <Line />
       <AboutImage fixed={image} />
+      <Text>
+        JavaScript is everywhere. In the old days, being a JavaScript developer
+        meant that you were a front end web developer. Forever bound to the
+        browser.
+      </Text>
+      <GoneText>Those days are gone.</GoneText>
+      <Text>
+        The rise of Node.js ushered in a new era. An era where being a
+        JavaScript developer doesn’t necessarily mean a front-end web developer.
+        As a JavaScript developer today, you can target more platforms than any
+        other high level language.
+      </Text>
     </AboutStyled>
   );
 }
