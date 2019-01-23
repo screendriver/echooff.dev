e43957907dd885429a5f75e72023f247e93e0c81
Christian Rackerseder <git@echooff.de>
2019-01-23 10:24:46 +0100

    ⚙️ Enable gatsby-transformer-json

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
