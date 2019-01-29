9c535491f71a87e0baa9e7ff41be5f4b0ae7c7fa
Christian Rackerseder <git@echooff.de>
2019-01-29 10:16:34 +0100

    🎨 Add theme-color

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { cyan } from '../colors';
 
 export function Head() {
   return (
     <Helmet>
+      <meta name="theme-color" content={cyan} />
       <link rel="shortcut icon" href="favicon.png" />
     </Helmet>
   );
