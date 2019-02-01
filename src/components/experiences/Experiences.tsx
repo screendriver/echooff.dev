5d5019f5e6abde4a356980af40cbef67306d5095
Christian Rackerseder <git@echooff.de>
2019-02-01 07:05:01 +0100

    💅 Style <Experiences />

---
+++ src/components/experiences/Experiences.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Section, SectionTheme } from '../Section';
+import { Circle } from './Circle';
+import { Description } from './Description';
+import { Experience } from '.';
+
+interface ExperiencesProps {
+  experiences: ReadonlyArray<Experience>;
+}
+
+const Timeline = styled.ul({
+  listStyleType: 'none',
+  position: 'relative',
+  padding: 0,
+  '::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    width: 2,
+    backgroundColor: '#eee',
+  },
+});
+
+const Moment = styled.li({
+  position: 'relative',
+  marginBottom: 100,
+});
+
+export function Experiences(props: ExperiencesProps) {
+  return (
+    <Section heading="Experience" theme={SectionTheme.Light}>
+      <Timeline>
+        {props.experiences.map(experience => (
+          <Moment>
+            <Description />
+            <Circle from={experience.from} to={experience.to} />
+          </Moment>
+        ))}
+      </Timeline>
+    </Section>
+  );
+}
