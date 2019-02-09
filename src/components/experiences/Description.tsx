01c6e7cf82ac762ec6877bf9ca147c02f586b70d
Christian Rackerseder <git@echooff.de>
2019-02-09 12:06:43 +0100

    👩‍🎤 Style <Description />

--- src/components/experiences/Description.tsx
+++ src/components/experiences/Description.tsx
@@ -1,16 +1,33 @@
 import React, { FC } from 'react';
+import styled from '@emotion/styled';
 import { Experience } from '.';
+import { black } from '../../colors';
 
 interface DescriptionProps {
   experience: Experience;
 }
 
+const Industry = styled.h4({
+  color: black,
+  textTransform: 'uppercase',
+  fontSize: 16,
+  fontWeight: 600,
+});
+
+const JobTitle = styled.h5({
+  color: '#629ca7',
+  fontSize: 20,
+  fontWeight: 600,
+});
+
+const JobDescription = styled.p({});
+
 export const Description: FC<DescriptionProps> = ({ experience }) => {
   return (
     <div>
-      <h1>{experience.industry}</h1>
-      <h2>{experience.jobTitle}</h2>
-      <p>{experience.jobDescription}</p>
+      <Industry>{experience.industry}</Industry>
+      <JobTitle>{experience.jobTitle}</JobTitle>
+      <JobDescription>{experience.jobDescription}</JobDescription>
     </div>
   );
 };
