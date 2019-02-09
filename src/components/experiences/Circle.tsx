afa1e08a2f7c86e8422677fdc189a4268bf4a5b6
Christian Rackerseder <git@echooff.de>
2019-02-09 12:07:39 +0100

    👦 Pass <Description /> as child to <Circle />

--- src/components/experiences/Circle.tsx
+++ src/components/experiences/Circle.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import { white } from '../../colors';
 
 interface ExperienceCircleProps {
   from: string;
   to: string;
+  childPosition: 'left' | 'right';
 }
+
+const Wrapper = styled.div({
+  position: 'relative',
+});
 
 const CircleStyled = styled.span({
   border: '1px solid #eee',
@@ -17,6 +22,7 @@ const CircleStyled = styled.span({
   textTransform: 'uppercase',
   width: 150,
   height: 150,
+  position: 'relative',
 });
 
 const Text = styled.span({
@@ -24,10 +30,27 @@ const Text = styled.span({
   lineHeight: '26px',
 });
 
-export function Circle(props: ExperienceCircleProps) {
+const ChildrenWrapper = styled.div(
+  (props: Pick<ExperienceCircleProps, 'childPosition'>) => {
+    const positionFlipped = props.childPosition === 'left' ? 'right' : 'left';
+    return {
+      position: 'absolute',
+      [positionFlipped]: 250,
+      width: 400,
+      textAlign: positionFlipped,
+    };
+  },
+);
+
+export const Circle: FC<ExperienceCircleProps> = props => {
   return (
-    <CircleStyled>
-      <Text>{props.to}</Text> <Text>-</Text> <Text>{props.from}</Text>
-    </CircleStyled>
+    <Wrapper>
+      <CircleStyled>
+        <Text>{props.to}</Text> <Text>-</Text> <Text>{props.from}</Text>
+        <ChildrenWrapper childPosition={props.childPosition}>
+          {props.children}
+        </ChildrenWrapper>
+      </CircleStyled>
+    </Wrapper>
   );
-}
+};
