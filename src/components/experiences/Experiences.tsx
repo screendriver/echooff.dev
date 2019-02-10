2606b46dc4cfe2f7b3a226e431e2641705b5246b
Christian Rackerseder <git@echooff.de>
2019-02-10 22:09:15 +0100

    🚚 Move page query into <Experiences /> as <StaticQuery />

--- src/components/experiences/Experiences.tsx
+++ src/components/experiences/Experiences.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
 import styled from '@emotion/styled';
 import { Section, SectionTheme } from '../Section';
 import { Circle } from './Circle';
 import { Description } from './Description';
 import { Experience } from '.';
 
-interface ExperiencesProps {
-  experiences: ReadonlyArray<Experience>;
+interface GraphQLData {
+  allExperienceJson: {
+    edges: [{ node: Experience }];
+  };
+}
+
+interface ExperiencesComponentProps {
+  data: GraphQLData;
 }
 
 const Timeline = styled.ul({
@@ -28,11 +35,20 @@ const Moment = styled.li({
   marginBottom: 100,
 });
 
-export function Experiences(props: ExperiencesProps) {
+function ExperiencesComponent({ data }: ExperiencesComponentProps) {
+  const experiences = data.allExperienceJson.edges.map<Experience>(
+    ({ node }) => ({
+      from: node.from,
+      to: node.to,
+      industry: node.industry,
+      jobTitle: node.jobTitle,
+      jobDescription: node.jobDescription,
+    }),
+  );
   return (
     <Section heading="Experience" theme={SectionTheme.Light}>
       <Timeline>
-        {props.experiences.map((experience, index) => (
+        {experiences.map((experience, index) => (
           <Moment key={index}>
             <Circle
               from={experience.from}
@@ -45,5 +61,30 @@ export function Experiences(props: ExperiencesProps) {
         ))}
       </Timeline>
     </Section>
+  );
+}
+
+const query = graphql`
+  query {
+    allExperienceJson {
+      edges {
+        node {
+          from
+          to
+          industry
+          jobTitle
+          jobDescription
+        }
+      }
+    }
+  }
+`;
+
+export function Experiences() {
+  return (
+    <StaticQuery
+      query={query}
+      render={(data: GraphQLData) => <ExperiencesComponent data={data} />}
+    />
   );
 }
