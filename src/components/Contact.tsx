650408690ea211ae969ab5e3c35f1144a8e44ab5
Christian Rackerseder <git@echooff.de>
2019-02-15 22:52:26 +0100

    ✨ Create <Contact /> component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Section, SectionTheme } from './Section';
+
+export function Contact() {
+  return (
+    <Section heading="Contact" theme={SectionTheme.Cyan}>
+      <p>Contact</p>
+    </Section>
+  );
+}
