0e7d565ae9a1569b86fe842bd916020082355565
Christian Rackerseder <git@echooff.de>
2019-02-15 22:53:03 +0100

    🗺 Add location / map to <Contact />

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { MapPin } from 'react-feather';
 import { Section, SectionTheme } from './Section';
 
 export function Contact() {
   return (
     <Section heading="Contact" theme={SectionTheme.Cyan}>
       <p>Contact</p>
+      <a href="https://www.openstreetmap.org/search?query=munich%20germany#map=11/48.1551/11.5418">
+        <MapPin />
+      </a>
     </Section>
   );
 }
