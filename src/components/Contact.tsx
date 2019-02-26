6c759012d47856c2acfbdb6cc9fa8154ebe29c7c
Christian Rackerseder <git@echooff.de>
2019-02-26 20:50:38 +0100

    👩‍🎤 Style <Contact />

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,23 +1,69 @@
 import React from 'react';
-import { MapPin, Twitter, GitHub, Linkedin } from 'react-feather';
+import {
+  MapPin,
+  MessageSquare,
+  Twitter,
+  GitHub,
+  Linkedin,
+} from 'react-feather';
+import styled from '@emotion/styled';
 import { Section, SectionTheme } from './Section';
+import { black, white } from '../colors';
+
+const LinkList = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  a: {
+    color: black,
+    textDecoration: 'none',
+  },
+});
+
+const SmallLinkList = styled(LinkList)({
+  marginTop: 50,
+});
+
+const BigLink = styled.a({
+  marginLeft: 50,
+  marginRight: 50,
+});
+
+const SmallLink = styled.a({
+  marginLeft: 20,
+  marginRight: 20,
+  svg: {
+    stroke: white,
+    transition: 'stroke 0.3s',
+  },
+  'svg:hover': {
+    stroke: black,
+  },
+});
 
 export function Contact() {
   return (
     <Section heading="Contact" theme={SectionTheme.Cyan}>
-      <p>Contact</p>
-      <a href="https://www.openstreetmap.org/search?query=munich%20germany#map=11/48.1551/11.5418">
-        <MapPin />
-      </a>
-      <a href="https://twitter.com/CallistoShip">
-        <Twitter />
-      </a>
-      <a href="https://github.com/screendriver">
-        <GitHub />
-      </a>
-      <a href="https://www.linkedin.com/in/christian-rackerseder-81a906177/">
-        <Linkedin />
-      </a>
+      <LinkList>
+        <BigLink href="https://www.openstreetmap.org/search?query=munich%20germany#map=11/48.1551/11.5418">
+          <MapPin />
+          <p>Munich, Germany</p>
+        </BigLink>
+        <BigLink href="threema://add?id=9TWBW4XN">
+          <MessageSquare />
+          <p>Threema ID: 9TWBW4XN</p>
+        </BigLink>
+      </LinkList>
+      <SmallLinkList>
+        <SmallLink href="https://twitter.com/CallistoShip">
+          <Twitter />
+        </SmallLink>
+        <SmallLink href="https://github.com/screendriver">
+          <GitHub color={white} />
+        </SmallLink>
+        <SmallLink href="https://www.linkedin.com/in/christian-rackerseder-81a906177/">
+          <Linkedin color={white} />
+        </SmallLink>
+      </SmallLinkList>
     </Section>
   );
 }
