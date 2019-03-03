1d7b462fdb0ea76073b649003b40d9e2ddbea0da
Christian Rackerseder <git@echooff.de>
2019-03-03 21:14:18 +0100

    💅 Style <ContactForm />

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,8 +5,57 @@ import React, {
   SetStateAction,
   FormEvent,
 } from 'react';
+import styled from '@emotion/styled';
 import ky from 'ky';
 import formurlencoded from 'form-urlencoded';
+import { white } from '../colors';
+
+const Heading = styled.h3({
+  color: white,
+  marginTop: 30,
+  marginBottom: 30,
+});
+
+const Form = styled.form({
+  margin: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  width: 500,
+});
+
+const commonStyle = {
+  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  color: '#2c3e50',
+  borderWidth: 0,
+  borderRadius: 4,
+  height: 34,
+  width: '47%',
+  padding: '6px 12px',
+};
+
+const Input = styled.input(commonStyle);
+
+const TextArea = styled.textarea({
+  ...commonStyle,
+  height: 'initial',
+  width: 'initial',
+  marginBottom: 20,
+});
+
+const Row = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginBottom: 20,
+});
+
+const Submit = styled.input({
+  backgroundColor: 'transparent',
+  border: `1px solid ${white}`,
+  color: white,
+  margin: '0 140px',
+  padding: '10px 20px',
+});
 
 interface Props {
   onFormSent(): void;
@@ -54,31 +103,33 @@ export function ContactForm(props: Props) {
   const [state, setState] = useState<State>(initialState);
   return (
     <>
-      <h3>Leave me a message</h3>
-      <form
+      <Heading>Leave me a message</Heading>
+      <Form
         name="contact"
         method="POST"
         data-netlify="true"
         onSubmit={handleSubmit(props, state, setState)}
       >
         <input type="hidden" name="form-name" value="contact" />
-        <input
-          name="name"
-          type="text"
-          placeholder="Name"
-          value={state.name}
-          required={true}
-          onChange={handleInputChange(setState)}
-        />
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          value={state.email}
-          required={true}
-          onChange={handleInputChange(setState)}
-        />
-        <textarea
+        <Row>
+          <Input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={state.name}
+            required={true}
+            onChange={handleInputChange(setState)}
+          />
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={state.email}
+            required={true}
+            onChange={handleInputChange(setState)}
+          />
+        </Row>
+        <TextArea
           name="message"
           rows={4}
           placeholder="Message"
@@ -86,8 +137,8 @@ export function ContactForm(props: Props) {
           required={true}
           onChange={handleInputChange(setState)}
         />
-        <input type="submit" value="Send Message" />
-      </form>
+        <Submit type="submit" value="Send Message" />
+      </Form>
     </>
   );
 }
