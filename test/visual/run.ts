8cfa5ac4e557c4d8fc655fe28c5117bcfdb78819
Christian Rackerseder <git@echooff.de>
2019-03-21 10:31:24 +0100

    🏃‍♂️ Run BackstopJS

--- test/visual/run.ts
+++ test/visual/run.ts
@@ -1,6 +1,8 @@
 import micro from 'micro';
 import handler from 'serve-handler';
 import listen from 'test-listen';
+import backstop from 'backstopjs';
+
 function createConfig(url: string) {
   return {
     config: {
@@ -73,7 +75,13 @@ async function run() {
     });
   });
 
-  await listen(server);
+  const url = await listen(server);
+  const config = createConfig(url);
+  try {
+    await backstop(process.argv[2], config);
+  } finally {
+    server.close();
+  }
 }
 
 run();
