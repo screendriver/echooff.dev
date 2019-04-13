f42b5207b5a030fba1d20f95921e6c81f4c26653
Christian Rackerseder <git@echooff.de>
2019-04-13 20:50:26 +0200

    ✅ Test config

---
+++ test/unit/shared/config.test.ts
@@ -0,0 +1,16 @@
+import { createConfig } from '../../../src/shared/config';
+
+afterEach(() => {
+  delete process.env.GATSBY_VISUAL_REGRESSION_TEST;
+});
+
+test('visualRegressionTest is true when GATSBY_VISUAL_REGRESSION_TEST is "true"', () => {
+  process.env.GATSBY_VISUAL_REGRESSION_TEST = 'true';
+  const { visualRegressionTest } = createConfig();
+  expect(visualRegressionTest).toEqual(true);
+});
+
+test('visualRegressionTest is false when GATSBY_VISUAL_REGRESSION_TEST is not set', () => {
+  const { visualRegressionTest } = createConfig();
+  expect(visualRegressionTest).toEqual(false);
+});
