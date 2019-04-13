bc4f8d47b31e593b69e9095e4e284564e56659d9
Christian Rackerseder <git@echooff.de>
2019-04-13 14:41:15 +0200

    🙈 Ignore "dist" directory

--- jest.config.js
+++ jest.config.js
@@ -140,9 +140,7 @@ module.exports = {
   testMatch: ['**/test/unit/**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
