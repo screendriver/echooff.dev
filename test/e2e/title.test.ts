d347cddf4b62d28b94ca6eccb04b8e070be6e2d2
Christian Rackerseder <git@echooff.de>
2019-04-17 20:39:21 +0200

    ✅ Fix linting error

--- test/e2e/title.test.ts
+++ test/e2e/title.test.ts
@@ -1,6 +1,6 @@
 import { Selector } from 'testcafe';
 
-fixture`Getting Started`.page`http://localhost:5000`;
+fixture('Getting Started').page('http://localhost:5000');
 
 test('Page title', async t => {
   const title = 'Christian Rackerseder - Full-Stack JavaScript Engineer';
