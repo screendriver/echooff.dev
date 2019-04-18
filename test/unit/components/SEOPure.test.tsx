1d8e3d146e7cd0e27d1ad91e945aa4bfd6707a01
Christian Rackerseder <git@echooff.de>
2019-04-18 22:08:06 +0200

    🧹 Use cleanup function

--- test/unit/components/SEOPure.test.tsx
+++ test/unit/components/SEOPure.test.tsx
@@ -2,7 +2,7 @@ import test from 'tape';
 import React from 'react';
 import { render } from 'react-testing-library';
 import Helmet from 'react-helmet';
-import '../../setup';
+import { withCleanup } from '../../cleanup';
 import { SEOPure, SEOPureProps } from '../../../src/components/SEOPure';
 
 const props: SEOPureProps = {
@@ -13,35 +13,47 @@ const props: SEOPureProps = {
   favicon: 'icon.png',
 };
 
-test('renders a title', async t => {
-  t.plan(1);
-  render(<SEOPure {...props} />);
-  const { title } = Helmet.peek();
-  t.equal(title, 'test title');
-});
+test(
+  'renders a title',
+  withCleanup(async t => {
+    t.plan(1);
+    render(<SEOPure {...props} />);
+    const { title } = Helmet.peek();
+    t.equal(title, 'test title');
+  }),
+);
 
-test('renders lang "en" HTML attribute', t => {
-  t.plan(1);
-  render(<SEOPure {...props} />);
-  const { htmlAttributes } = Helmet.peek();
-  t.deepEqual(htmlAttributes, { lang: 'en' });
-});
+test(
+  'renders lang "en" HTML attribute',
+  withCleanup(t => {
+    t.plan(1);
+    render(<SEOPure {...props} />);
+    const { htmlAttributes } = Helmet.peek();
+    t.deepEqual(htmlAttributes, { lang: 'en' });
+  }),
+);
 
-test('renders a favicon', t => {
-  t.plan(1);
-  render(<SEOPure {...props} />);
-  const { linkTags } = Helmet.peek() as any;
-  t.deepEqual(linkTags, [{ rel: 'shortcut icon', href: 'icon.png' }]);
-});
+test(
+  'renders a favicon',
+  withCleanup(t => {
+    t.plan(1);
+    render(<SEOPure {...props} />);
+    const { linkTags } = Helmet.peek() as any;
+    t.deepEqual(linkTags, [{ rel: 'shortcut icon', href: 'icon.png' }]);
+  }),
+);
 
-test('renders meta tags', t => {
-  t.plan(1);
-  render(<SEOPure {...props} />);
-  const { metaTags } = Helmet.peek() as any;
-  t.deepEqual(metaTags, [
-    { name: 'theme-color', content: '#7bc3d1' },
-    { name: 'description', content: 'my description' },
-    { name: 'keywords', content: 'key,words' },
-    { name: 'author', content: 'me' },
-  ]);
-});
+test(
+  'renders meta tags',
+  withCleanup(t => {
+    t.plan(1);
+    render(<SEOPure {...props} />);
+    const { metaTags } = Helmet.peek() as any;
+    t.deepEqual(metaTags, [
+      { name: 'theme-color', content: '#7bc3d1' },
+      { name: 'description', content: 'my description' },
+      { name: 'keywords', content: 'key,words' },
+      { name: 'author', content: 'me' },
+    ]);
+  }),
+);
