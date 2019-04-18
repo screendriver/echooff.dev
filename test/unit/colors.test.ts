690c6cd1b05146952ce112e50773d06ca9418bec
Christian Rackerseder <git@echooff.de>
2019-04-18 21:55:12 +0200

    ♻️ Rewrite tests from using Jest to tape

--- test/unit/colors.test.ts
+++ test/unit/colors.test.ts
@@ -1,29 +1,37 @@
+import test from 'tape';
 import * as colors from '../../src/colors';
 
-test('grey', () => {
-  expect(colors.grey).toEqual('#656c6d');
+test('grey', t => {
+  t.plan(1);
+  t.equal(colors.grey, '#656c6d');
 });
 
-test('darkGrey', () => {
-  expect(colors.darkGrey).toEqual('#7d8384');
+test('darkGrey', t => {
+  t.plan(1);
+  t.equal(colors.darkGrey, '#7d8384');
 });
 
-test('white', () => {
-  expect(colors.white).toEqual('#ffffff');
+test('white', t => {
+  t.plan(1);
+  t.equal(colors.white, '#ffffff');
 });
 
-test('darkerWhite', () => {
-  expect(colors.darkerWhite).toEqual('#f4f5f6');
+test('darkerWhite', t => {
+  t.plan(1);
+  t.equal(colors.darkerWhite, '#f4f5f6');
 });
 
-test('black', () => {
-  expect(colors.black).toEqual('#121d1f');
+test('black', t => {
+  t.plan(1);
+  t.equal(colors.black, '#121d1f');
 });
 
-test('cyan', () => {
-  expect(colors.cyan).toEqual('#7bc3d1');
+test('cyan', t => {
+  t.plan(1);
+  t.equal(colors.cyan, '#7bc3d1');
 });
 
-test('light', () => {
-  expect(colors.light).toEqual('#f6f6f6');
+test('light', t => {
+  t.plan(1);
+  t.equal(colors.light, '#f6f6f6');
 });
