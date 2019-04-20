e0a827123afc8e1e4a6731b8ae87aa4e157ea612
Christian Rackerseder <git@echooff.de>
2019-04-20 15:40:58 +0200

    🚚 Move <About /> component to different directory

---
+++ src/components/about/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+import { FixedObject } from 'gatsby-image';
+import { AboutUi } from './ui';
+
+interface GraphQLData {
+  aboutFile: {
+    childImageSharp: {
+      fixed: FixedObject;
+    };
+  };
+}
+
+const query = graphql`
+  query {
+    aboutFile: file(name: { eq: "about" }, extension: { eq: "jpg" }) {
+      childImageSharp {
+        fixed(quality: 75, width: 200) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+  }
+`;
+
+function renderAboutUi(data: GraphQLData) {
+  return <AboutUi fixedImage={data.aboutFile.childImageSharp.fixed} />;
+}
+
+export function About() {
+  return <StaticQuery query={query} render={renderAboutUi} />;
+}
