3f7e5631bce8147179071c63c2c88ff42c39627c
Christian Rackerseder <git@echooff.de>
2019-04-20 23:16:10 +0200

    🔨 Fix test description

--- test/e2e/about.test.ts
+++ test/e2e/about.test.ts
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 fixture('About image').page('http://localhost:5000');
 
-test('About image width', async t => {
+test('width', async t => {
   const image = Selector('img[alt="My face"]');
   await t.expect(image.getAttribute('width')).eql('200');
 });
