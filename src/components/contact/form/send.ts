80b55ca1507bfe7bf2cbf5110f3d2a226dbd0bcc
Christian Rackerseder <git@echooff.de>
2019-04-21 11:33:50 +0200

    ✨ Create function to send form values

---
+++ src/components/contact/form/send.ts
@@ -0,0 +1,15 @@
+import ky from 'ky';
+import formurlencoded from 'form-urlencoded';
+
+interface FormValues {
+  name: string;
+  email: string;
+  message: string;
+}
+
+export async function sendForm(formValues: FormValues) {
+  await ky.post('/', {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: formurlencoded({ 'form-name': 'contact', ...formValues }),
+  });
+}
