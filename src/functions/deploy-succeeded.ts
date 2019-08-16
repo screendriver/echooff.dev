ae747973e8a4fb224ad7d6498472507e53293367
Christian Rackerseder <git@echooff.de>
2019-08-16 13:37:15 +0200

    🗑 Remove not needed deploy-succeeded function

--- src/functions/deploy-succeeded.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import got from 'got';
-
-export const handler: APIGatewayProxyHandler = async () => {
-  const { statusCode, body } = await got.post(
-    `https://maker.ifttt.com/trigger/netlify-deploy-succeeded/with/key/${process.env.IFTTT_NETLIFY_DEPLOY_SUCCEEDED_KEY}`,
-  );
-  return { statusCode, body };
-};
