d1fd65f7cedae8b388f286b7244a5cf2de732a70
Christian Rackerseder <git@echooff.de>
2019-08-24 05:46:45 +0200

    ✅ Test if about image exists

--- test/e2e/about.test.ts
+++ test/e2e/about.test.ts
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 fixture('About image').page('http://localhost:5000');
 
-test('width', async t => {
+test('exists', async t => {
   const image = Selector('img[alt="My face"]');
-  await t.expect(image.getAttribute('width')).eql('200');
+  await t.expect(image.exists).ok();
 });
