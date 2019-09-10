84a9c67e385128afe848a55754fc176aa8dde220
Christian Rackerseder <git@echooff.de>
2019-09-10 22:04:55 +0200

    ✅ First visual test with Percy

---
+++ test/visual/main.test.js
@@ -0,0 +1,10 @@
+import percySnapshot from '@percy/testcafe';
+
+fixture('Visual').page('http://localhost:9000');
+
+test('main page', async t => {
+  await t.wait(1000);
+  await percySnapshot(t, 'Main page', {
+    widths: [320, 375, 425, 768, 1024, 1440, 1920],
+  });
+});
