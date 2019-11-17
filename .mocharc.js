0b0bdbb58dcd912944b7befa092cbe601354b0df
Christian Rackerseder <git@echooff.de>
2019-11-17 14:12:30 +0100

    🏎 Use TypeScript's faster transpile module when running tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
   spec: 'test/unit/**/*.test.*',
-  require: ['esm', 'ts-node/register', './test/setup.ts'],
+  require: ['esm', 'ts-node/register/transpile-only', './test/setup.ts'],
   extension: ['ts', 'tsx'],
   ui: 'tdd',
 };
