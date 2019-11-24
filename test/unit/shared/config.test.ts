a1a70b0c0cb253bd2a60a09aa723882eabec3312
Christian Rackerseder <git@echooff.de>
2019-11-24 21:19:14 +0100

    💄 Use Chai's TDD assert style

--- test/unit/shared/config.test.ts
+++ test/unit/shared/config.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { assert } from 'chai';
 import { createConfig } from '../../../src/shared/config';
 
 suite('config', () => {
@@ -9,11 +9,11 @@ suite('config', () => {
   test('visualRegressionTest is true when GATSBY_VISUAL_REGRESSION_TEST is "true"', () => {
     process.env.GATSBY_VISUAL_REGRESSION_TEST = 'true';
     const { visualRegressionTest } = createConfig();
-    expect(visualRegressionTest).to.equal(true);
+    assert.isTrue(visualRegressionTest);
   });
 
   test('visualRegressionTest is false when GATSBY_VISUAL_REGRESSION_TEST is not set', () => {
     const { visualRegressionTest } = createConfig();
-    expect(visualRegressionTest).to.equal(false);
+    assert.isFalse(visualRegressionTest);
   });
 });
