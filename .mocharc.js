d67c6fcdec56d8c784c4a6137b94cbea11b07149
Christian Rackerseder <git@echooff.de>
2020-05-27 21:41:27 +0200

    🚫 Don't require esm in Mocha

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
   spec: 'test/unit/**/*.test.*',
-  require: ['esm', 'ts-node/register/transpile-only', './test/setup.ts'],
+  require: ['ts-node/register/transpile-only', './test/setup.ts'],
   extension: ['ts', 'tsx'],
   ui: 'tdd',
   reporter: 'dot',
