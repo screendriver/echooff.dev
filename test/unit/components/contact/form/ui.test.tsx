d2510587b58e26ff05280df12b5ed08a08fc2c84
Christian Rackerseder <git@echooff.de>
2020-06-11 20:14:55 +0200

    ✅ Fix broken tests since translations

--- test/unit/components/contact/form/ui.test.tsx
+++ test/unit/components/contact/form/ui.test.tsx
@@ -18,33 +18,41 @@ function renderFormUi(overrides: Partial<Props>) {
 }
 
 suite('<Contact />', function () {
+  test('renders a translated heading', function () {
+    const { queryByText } = renderFormUi({});
+    const headingElement = queryByText('contact.leave_a_message');
+    assert.isNotNull(headingElement);
+  });
+
   test('render given name in an input', function () {
     const { getByPlaceholderText } = renderFormUi({ name: 'My name' });
-    const input = getByPlaceholderText('Name') as HTMLInputElement;
+    const input = getByPlaceholderText('contact.name') as HTMLInputElement;
     assert.equal(input.value, 'My name');
   });
 
   test('render given email in an input', function () {
     const { getByPlaceholderText } = renderFormUi({ email: 'foo@example.com' });
-    const input = getByPlaceholderText('Email') as HTMLInputElement;
+    const input = getByPlaceholderText('contact.email') as HTMLInputElement;
     assert.equal(input.value, 'foo@example.com');
   });
 
   test('render given message in a textarea', function () {
     const { getByPlaceholderText } = renderFormUi({ message: 'My message' });
-    const textarea = getByPlaceholderText('Message') as HTMLTextAreaElement;
+    const textarea = getByPlaceholderText(
+      'contact.message',
+    ) as HTMLTextAreaElement;
     assert.equal(textarea.value, 'My message');
   });
 
   test('render a enabled submit button', function () {
     const { getByDisplayValue } = renderFormUi({ submitDisabled: false });
-    const submit = getByDisplayValue('Send Message') as HTMLInputElement;
+    const submit = getByDisplayValue('contact.submit') as HTMLInputElement;
     assert.isFalse(submit.disabled);
   });
 
   test('render a disabled submit button', function () {
     const { getByDisplayValue } = renderFormUi({ submitDisabled: true });
-    const submit = getByDisplayValue('Send Message') as HTMLInputElement;
+    const submit = getByDisplayValue('contact.submit') as HTMLInputElement;
     assert.isTrue(submit.disabled);
   });
 });
