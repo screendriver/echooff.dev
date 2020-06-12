c5558156a1e81360c4d1c7c23fe6c898ef39b418
Christian Rackerseder <git@echooff.de>
2020-06-12 14:20:11 +0200

    🌐 Intercept /quickmetrics HTTP request

--- test/e2e/about.test.ts
+++ test/e2e/about.test.ts
@@ -1,6 +1,12 @@
-import { Selector } from 'testcafe';
+import { Selector, RequestMock } from 'testcafe';
 
-fixture('About image').page('http://localhost:9000');
+const quickmetricsMock = RequestMock()
+  .onRequestTo('http://localhost:9000/.netlify/functions/quickmetrics')
+  .respond('');
+
+fixture('About image')
+  .page('http://localhost:9000')
+  .requestHooks(quickmetricsMock);
 
 test('exists', async (t) => {
   const image = Selector('img[alt="My face"]');
