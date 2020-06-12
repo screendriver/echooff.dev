e9faa927a543a56c9f7cf6ce2933c0bd4e376736
Christian Rackerseder <git@echooff.de>
2020-06-12 16:58:11 +0200

    📚 Write some Storybook stories

---
+++ src/components/Navigation.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navigation } from './Navigation';
+
+export default { title: 'Organisms', component: Navigation };
+
+export function navigation() {
+  return <Navigation />;
+}
