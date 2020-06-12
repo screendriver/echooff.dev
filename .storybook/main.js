1ffef1c11499cc324fe5b476ad4e883210505e27
Christian Rackerseder <git@echooff.de>
2020-06-12 16:36:56 +0200

    🏳️‍🌈 Initialise i18next in Storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,11 @@ module.exports = {
       loader: require.resolve('babel-loader'),
       options: {
         presets: [['react-app', { flow: false, typescript: true }]],
+        plugins: [
+          require.resolve('@babel/plugin-proposal-class-properties'),
+          // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+          require.resolve('babel-plugin-remove-graphql-queries'),
+        ],
       },
     });
     config.resolve.extensions.push('.ts', '.tsx');
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,29 @@
-import { addParameters } from '@storybook/client-api';
+import React, { Suspense } from 'react';
+import i18next from 'i18next';
+import HttpApi from 'i18next-http-backend';
+import { initReactI18next, I18nextProvider } from 'react-i18next';
+import { addParameters, addDecorator } from '@storybook/client-api';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+i18next
+  .use(HttpApi)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  });
 
 addParameters({
   viewport: {
     viewports: INITIAL_VIEWPORTS,
   },
 });
+
+addDecorator((story) => (
+  <I18nextProvider i18next={i18next}>
+    <Suspense fallback={<div>loading...</div>}>{story()}</Suspense>
+  </I18nextProvider>
+));
