99d619bc7b331bfd5ff588a65eb53414b68058b8
Christian Rackerseder <git@echooff.de>
2020-10-21 08:25:03 +0200

    chore(storybook): remove storybook stories

--- src/components/Navigation.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigation } from './Navigation';
-
-export default { title: 'Organisms', component: Navigation };
-
-export function navigation() {
-  return <Navigation />;
-}
--- src/components/YetiSVG.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { YetiSVG } from './YetiSVG';
-
-export default { title: 'Atoms', component: YetiSVG };
-
-export const yetisvg = () => <YetiSVG />;
