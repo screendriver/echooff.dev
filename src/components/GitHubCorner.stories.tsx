d55709b5ca352fae289a6230cee5e0e07eb681fd
Christian Rackerseder <git@echooff.de>
2020-10-21 08:25:03 +0200

    chore(storybook): remove storybook stories

--- src/components/GitHubCorner.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { GitHubCorner } from './GitHubCorner';
-
-export default { title: 'Atoms', component: GitHubCorner };
-
-export const gitub_corner = () => <GitHubCorner />;
