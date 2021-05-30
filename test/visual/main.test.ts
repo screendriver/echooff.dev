000d6e4a42ba91b20b445de7ba36e872478e7023
Christian Rackerseder <git@echooff.de>
2021-05-30 19:08:18 +0200

    test: fix compile error in visual regression tests

--- test/visual/main.test.ts
+++ test/visual/main.test.ts
@@ -1,5 +1,5 @@
 import { RequestMock } from 'testcafe';
-import percySnapshot from '@percy/testcafe';
+import * as percySnapshot from '@percy/testcafe';
 
 const quickmetricsMock = RequestMock()
   .onRequestTo('http://localhost:9000/.netlify/functions/quickmetrics')
@@ -9,7 +9,8 @@ fixture('Visual').page('http://localhost:9000').requestHooks(quickmetricsMock);
 
 test('main page', async (t) => {
   await t.wait(1000);
-  await percySnapshot('Main page', {
+  // @ts-expect-error https://github.com/percy/percy-testcafe/issues/109 and https://github.com/percy/percy-testcafe/issues/108
+  await percySnapshot(t, 'Main page', {
     widths: [320, 375, 425, 768, 1024, 1440, 1920],
   });
 });
