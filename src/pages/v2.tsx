c2e740849f6aefd48decddee71e63d02383e4192
Christian Rackerseder <git@echooff.de>
2021-11-01 15:41:59 +0100

    feat: create first basic /v2 route

---
+++ src/pages/v2.tsx
@@ -0,0 +1,46 @@
+import { Fragment, FunctionComponent } from 'react';
+import { graphql, PageProps } from 'gatsby';
+import { Header } from '../v2/Header';
+
+interface DataType {
+  readonly site: {
+    readonly siteMetadata: {
+      readonly author: string;
+      readonly jobTitle: string;
+      readonly keywords: string;
+      readonly favicon: string;
+    };
+  };
+}
+
+type V2PageProps = PageProps<DataType>;
+
+export const query = graphql`
+  query V2 {
+    site {
+      siteMetadata {
+        author
+        jobTitle
+        keywords
+        favicon
+      }
+    }
+  }
+`;
+
+const V2Page: FunctionComponent<V2PageProps> = ({ data }) => {
+  const { author, jobTitle, keywords, favicon } = data.site.siteMetadata;
+  return (
+    <Fragment>
+      <Header
+        title={`${author} - ${jobTitle}`}
+        description={jobTitle}
+        author={author}
+        keywords={keywords}
+        favicon={favicon}
+      />
+    </Fragment>
+  );
+};
+
+export default V2Page;
