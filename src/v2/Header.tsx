dd1ab902bc0dd36992b6f46aac5be97792cf1168
Christian Rackerseder <git@echooff.de>
2021-11-01 15:42:26 +0100

    feat: create basic <Header /> component

---
+++ src/v2/Header.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'react';
+import { Helmet } from 'react-helmet';
+
+export interface HeaderProps {
+  title: string;
+  description: string;
+  author: string;
+  keywords: string;
+  favicon: string;
+}
+
+export const Header: FunctionComponent<HeaderProps> = ({
+  title,
+  description,
+  author,
+  keywords,
+  favicon,
+}) => {
+  return (
+    <Helmet title={title}>
+      <html lang="en" />
+      <link rel="shortcut icon" href={favicon} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author} />
+    </Helmet>
+  );
+};
