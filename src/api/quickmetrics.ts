5f2059f495fa5a355bb315048e526d540b62081d
Christian Rackerseder <git@echooff.de>
2021-11-01 13:52:00 +0100

    refactor: call real quickmetrics endpoint only in production

--- src/api/quickmetrics.ts
+++ src/api/quickmetrics.ts
@@ -5,15 +5,17 @@ export default async function (
   _request: GatsbyFunctionRequest,
   response: GatsbyFunctionResponse,
 ): Promise<void> {
-  await got.post('https://qckm.io/json', {
-    headers: {
-      'x-qm-key': process.env.QUICKMETRICS_API_KEY,
-    },
-    json: {
-      name: 'echooff.dev',
-      value: 1,
-      dimension: 'GET /',
-    },
-  });
+  if (process.env.NODE_ENV === 'production') {
+    await got.post('https://qckm.io/json', {
+      headers: {
+        'x-qm-key': process.env.QUICKMETRICS_API_KEY,
+      },
+      json: {
+        name: 'echooff.dev',
+        value: 1,
+        dimension: 'GET /',
+      },
+    });
+  }
   response.status(200).send('');
 }
