91033b12f4df365964ffac550bf86d59169428e8
Christian Rackerseder <git@echooff.de>
2021-11-03 06:16:27 +0100

    chore: extend color palette with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'light-pink': '#FFADAD',
+        'deep-champagne': '#FFD6A5',
+        'lemon-yellow-crayola': '#FDFFB6',
+        'tea-green': '#CAFFBF',
+        celeste: '#9BF6FF',
+        'baby-blue-eyes': '#A0C4FF',
+        'maximum-blue-purple': '#BDB2FF',
+        mauve: '#FFC6FF',
+        'baby-powder': '#FFFFFC',
+      },
+    },
+  },
+};
