03ccdd96b6a07a6c2ea0e8d80c3fd2dd80f4fbac
Christian Rackerseder <git@echooff.de>
2021-11-06 16:37:02 +0100

    chore: remove unused module

--- src/shared/config.ts
+++
@@ -1,9 +0,0 @@
-export interface Config {
-  visualRegressionTest: boolean;
-}
-
-export function createConfig(): Config {
-  return {
-    visualRegressionTest: process.env.GATSBY_VISUAL_REGRESSION_TEST === 'true',
-  };
-}
