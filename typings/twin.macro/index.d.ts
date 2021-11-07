99c2b069dadb1bccaa3132f4c885caae41f4b1bc
Christian Rackerseder <git@echooff.de>
2021-11-07 20:24:19 +0100

    chore: remove not used typings

--- typings/twin.macro/index.d.ts
+++
@@ -1,8 +0,0 @@
-import 'twin.macro';
-import emotionStyled from '@emotion/styled';
-import { css as emotionReactCss } from '@emotion/react';
-
-declare module 'twin.macro' {
-  const styled: typeof emotionStyled;
-  const css: typeof emotionReactCss;
-}
