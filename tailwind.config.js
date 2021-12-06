8788c1cc18850333481469aca4ffd544b2517308
Christian Rackerseder <git@echooff.de>
2021-12-06 22:44:18 +0100

    chore: remove not needed color overrides

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
             fontFamily: {
                 'jetbrains-mono': '"JetBrains Mono"',
             },
-            colors: {
-                'light-pink': '#FFADAD',
-                'deep-champagne': '#FFD6A5',
-                'lemon-yellow-crayola': '#FDFFB6',
-                'tea-green': '#CAFFBF',
-                celeste: '#9BF6FF',
-                'baby-blue-eyes': '#A0C4FF',
-                'maximum-blue-purple': '#BDB2FF',
-                mauve: '#FFC6FF',
-                'baby-powder': '#FFFFFC',
-            },
         },
     },
 };
