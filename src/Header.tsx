c8075d14cea376eb35829305b297a2e216761508
Christian Rackerseder <git@echooff.de>
2021-12-06 23:50:43 +0100

    feat: put a gradient behind "Full-Stack JavaScript Engineer"

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,9 +16,11 @@ export const Header: FunctionComponent = () => {
                 imgClassName="mix-blend-luminosity"
             />
             <section className="absolute top-0 w-full h-full flex flex-col justify-center items-center">
-                <div className="text-dracula-light flex flex-col bg-dracula-dark bg-opacity-80 p-2 shadow rounded-xl sm:p-4 lg:p-7">
-                    <h1 className="text-lg md:text-2xl lg:text-5xl">Hello, I'm Christian</h1>
-                    <h2 className="text-sm md:text-lg lg:text-2xl mt-3">Full-Stack JavaScript Engineer</h2>
+                <div className="text-dracula-light bg-dracula-dark bg-opacity-80 p-2 shadow rounded-xl sm:p-4 lg:p-7">
+                    <h1 className="text-lg md:mb-2 lg:mb-4 md:text-2xl lg:text-5xl">Hello, I'm Christian</h1>
+                    <h2 className="text-sm md:text-lg lg:text-2xl mt-3 text-transparent bg-clip-text bg-gradient-to-r from-dracula-pink to-dracula">
+                        Full-Stack JavaScript Engineer
+                    </h2>
                 </div>
             </section>
         </header>
