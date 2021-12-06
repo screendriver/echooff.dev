099cd7b64f04592405422517fa1d31c028a0f16e
Christian Rackerseder <git@echooff.de>
2021-12-06 22:46:29 +0100

    refactor: use Dracula colors for <Header />

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 export const Header: FunctionComponent = () => {
     return (
-        <header className="relative bg-gradient-to-b from-lemon-yellow-crayola to-celeste">
+        <header className="relative bg-gradient-to-b from-dracula-cyan">
             <StaticImage
                 alt="Christian Rackerseder"
                 src="./img/header-15.jpg"
@@ -16,7 +16,7 @@ export const Header: FunctionComponent = () => {
                 imgClassName="mix-blend-luminosity"
             />
             <section className="absolute top-0 w-full h-full flex flex-col justify-center items-center">
-                <div className="text-baby-powder font-jetbrains-mono flex flex-col bg-gray-700 bg-opacity-80 p-2 shadow rounded-xl sm:p-4 lg:p-7">
+                <div className="text-dracula-light flex flex-col bg-dracula-dark bg-opacity-80 p-2 shadow rounded-xl sm:p-4 lg:p-7">
                     <h1 className="text-lg md:text-2xl lg:text-5xl">Hello, I'm Christian</h1>
                     <h2 className="text-sm md:text-lg lg:text-2xl mt-3">Full-Stack JavaScript Engineer</h2>
                 </div>
