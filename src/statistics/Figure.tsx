60ce0c6af5623234f36fc40e435da402a9319039
Christian Rackerseder <git@echooff.de>
2021-12-25 08:42:22 +0100

    chore: apply different styling to <Figure /> component

--- src/statistics/Figure.tsx
+++ src/statistics/Figure.tsx
@@ -6,7 +6,7 @@ interface FigureProps {
 
 export const Figure: FunctionComponent<FigureProps> = (props) => {
     return (
-        <div className="flex flex-col justify-center items-center bg-dracula-darker rounded h-20 lg:h-28 text-sm sm:text-base lg:text-lg xl:text-xl text-center">
+        <div className="flex flex-col justify-center items-center bg-dracula-darker rounded h-20 lg:h-28 lg:text-lg text-center">
             <p className="text-dracula-light">{props.description}</p>
             {props.children}
         </div>
