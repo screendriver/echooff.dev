1ab0f5e57f9e3686110c32a63edb669065798a3a
Christian Rackerseder <git@echooff.de>
2022-02-12 16:09:50 +0100

    feat: format "since" date to a human readable date

---
+++ src/resume/date.ts
@@ -0,0 +1,12 @@
+import Result, { err, ok } from 'true-myth/result';
+import { format, parseISO } from 'date-fns/fp';
+
+export function formatSinceDate(since: string, onlyYear: boolean): Result<string, string> {
+    try {
+        const formatString = onlyYear ? 'yyyy' : 'MMMM yyyy';
+        const formatToString = format(formatString);
+        return ok(formatToString(parseISO(since)));
+    } catch {
+        return err(`Since "${since}" is not a valid date`);
+    }
+}
