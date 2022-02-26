82ec7ff7c298a5acd6bf837209c210f1876b035c
Christian Rackerseder <git@echooff.de>
2022-02-26 08:54:29 +0100

    feat: create /contact-form static server route

---
+++ src/static-server/contact-form-route.ts
@@ -0,0 +1,20 @@
+import { RouteOptions } from 'fastify';
+
+export function createContactFormRoute(): RouteOptions {
+    return {
+        method: 'POST',
+        url: '/contact-form',
+        schema: {
+            headers: {
+                'content-type': { type: 'string', const: 'application/x-www-form-urlencoded' },
+            },
+            body: {},
+            response: {
+                200: {},
+            },
+        },
+        async handler(_request, reply) {
+            return reply.send({});
+        },
+    };
+}
