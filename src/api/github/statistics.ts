9465dbc3833dd957f308508f8ef6234673903b61
Christian Rackerseder <git@echooff.de>
2022-03-05 17:23:25 +0100

    ci: rename GATSBY_GITHUB_BASE_URL to GATSBY_GITHUB_API_BASE_URL

--- src/api/github/statistics.ts
+++ src/api/github/statistics.ts
@@ -7,7 +7,7 @@ import { gitHubStatisticsSchema } from '../../statistics/statistics-schema';
 export default async function (_request: GatsbyFunctionRequest, response: GatsbyFunctionResponse): Promise<void> {
     const gitHubStatisticsResponse = await fetchGitHubStatistics({
         graphql,
-        gitHubBaseUrl: gitHubBaseUrlSchema.parse(process.env.GATSBY_GITHUB_BASE_URL),
+        gitHubBaseUrl: gitHubBaseUrlSchema.parse(process.env.GATSBY_GITHUB_API_BASE_URL),
         gitHubLogin: gitHubLoginSchema.parse(process.env.GATSBY_GITHUB_LOGIN),
         gitHubApiToken: gitHubApiTokenSchema.parse(process.env.GATSBY_GITHUB_API_TOKEN),
     });
