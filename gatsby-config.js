5f32f87a45061aa3376468eccc3987521a8abc21
Christian Rackerseder <git@echooff.de>
2022-03-11 08:54:15 +0100

    chore: add new keyword

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     siteMetadata: {
         author: 'Christian Rackerseder',
         jobTitle: 'Full-Stack JavaScript Engineer',
-        keywords: 'TypeScript,JavaScript,HTML,CSS,Node.js,React',
+        keywords: 'Racki,TypeScript,JavaScript,HTML,CSS,Node.js,React',
         favicon: 'favicon.png',
         siteUrl: 'https://www.echooff.dev',
     },
