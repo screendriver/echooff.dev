482a3fa395e784f7841baead6b6b5fbe65dee54e
Christian Rackerseder <git@echooff.de>
2022-03-12 12:52:27 +0100

    ci: create redirects for Netlify functions

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 exports.createPages = ({ actions }) => {
     const { createRedirect } = actions;
+    createRedirect({ fromPath: '/api/*', toPath: '/.netlify/functions/__api', statusCode: 200 });
     // https://github.com/netlify/netlify-plugin-gatsby/issues/219#issuecomment-1018321552
     createRedirect({ fromPath: '/*', toPath: '/404.html', statusCode: 404 });
 };
