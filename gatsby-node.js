7f598f24845a7d21447484bd5df59c64e12c26e9
Christian Rackerseder <git@echooff.de>
2022-03-12 13:46:19 +0100

    ci: define redirects in netlify.toml

--- gatsby-node.js
+++
@@ -1,6 +0,0 @@
-exports.createPages = ({ actions }) => {
-    const { createRedirect } = actions;
-    createRedirect({ fromPath: '/api/*', toPath: '/.netlify/functions/__api', statusCode: 200 });
-    // https://github.com/netlify/netlify-plugin-gatsby/issues/219#issuecomment-1018321552
-    createRedirect({ fromPath: '/*', toPath: '/404.html', statusCode: 404 });
-};
