1a4a59ca17ff05459672f489022199eb4a064908
Christian Rackerseder <git@echooff.de>
2022-03-24 09:52:41 +0100

    Create very first visual regression test

---
+++ test/visual-regression/main.test.ts
@@ -0,0 +1,19 @@
+import { chromium } from 'playwright';
+import percySnapshot from '@percy/playwright';
+
+async function run(): Promise<void> {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8000', { waitUntil: 'networkidle' });
+
+    await percySnapshot(page, 'echooff.dev main');
+
+    await browser.close();
+}
+
+function crash(error: unknown): void {
+    console.error(error);
+    process.exitCode = 1;
+}
+
+run().catch(crash);
