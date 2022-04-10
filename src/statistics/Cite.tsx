69c85ded7ab1e841af0fb337df45e3c978800367
Christian Rackerseder <git@echooff.de>
2022-04-10 07:36:04 +0200

    Use PropsWithChildren interface to specify children
    See also https://github.com/DefinitelyTyped/DefinitelyTyped/pull/56210

--- src/statistics/Cite.tsx
+++ src/statistics/Cite.tsx
@@ -1,10 +1,10 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, PropsWithChildren } from 'react';
 
 interface CiteProps {
     'aria-label'?: string;
 }
 
-export const Cite: FunctionComponent<CiteProps> = (props) => {
+export const Cite: FunctionComponent<PropsWithChildren<CiteProps>> = (props) => {
     return (
         <cite aria-label={props['aria-label']} className="text-lg text-dracula-green font-bold not-italic mt-2">
             {props.children}
--- src/statistics/Figure.tsx
+++ src/statistics/Figure.tsx
@@ -1,10 +1,10 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, PropsWithChildren } from 'react';
 
 interface FigureProps {
     readonly description: string;
 }
 
-export const Figure: FunctionComponent<FigureProps> = (props) => {
+export const Figure: FunctionComponent<PropsWithChildren<FigureProps>> = (props) => {
     return (
         <div className="flex flex-col justify-center items-center bg-dracula-darker rounded h-20 lg:h-28 lg:text-lg text-center">
             <p className="text-dracula-light">{props.description}</p>
