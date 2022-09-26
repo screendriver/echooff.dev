e843a80ad82e9b8f244ab55c64fa1abd081e46b1
Christian Rackerseder <git@echooff.de>
2022-09-26 13:23:31 +0200

    Configure Tailwind CSS to respect *.astro files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         letterSpacing: {
             widest: '.25em',
