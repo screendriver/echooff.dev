41025355b310e411101635965117305f1d3ed559
Christian Rackerseder <git@echooff.de>
2022-09-28 09:13:41 +0200

    Create statistics in Astro and Svelte components

---
+++ src/components/statistics/GitHubStars.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { icons } from 'feather-icons';
+    import Cite from './Cite.svelte';
+    import Figure from './Figure.svelte';
+    import LoadingSpinner from './LoadingSpinner.svelte';
+    import type { StatisticsStateMachineState } from './state-machine';
+
+    export let state: StatisticsStateMachineState;
+</script>
+
+<Figure description="GitHub Stars">
+    <Cite ariaLabel="GitHub Stars">
+        {#if state.matches('loaded')}
+            {state.context.gitHubStatistics.value.user.starredRepositories.totalCount}
+        {:else if state.matches('failed')}
+            {@html icons['alert-triangle']?.toSvg()}
+        {:else}
+            <LoadingSpinner />
+        {/if}
+    </Cite>
+</Figure>
---
+++ src/components/statistics/YearsInBusiness.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { StatisticsStateMachineState } from './state-machine';
+    import Figure from './Figure.svelte';
+    import Cite from './Cite.svelte';
+
+    export let state: StatisticsStateMachineState;
+</script>
+
+{#if state.context.yearsOfExperience.isJust}
+    <Figure description="Experience">
+        <Cite ariaLabel="Years of experience">{state.context.yearsOfExperience.value} yrs</Cite>
+    </Figure>
+{/if}
