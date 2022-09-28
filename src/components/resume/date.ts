f05cd644b293100d358c2b7f66ea9a2e80406bd3
Christian Rackerseder <git@echooff.de>
2022-09-28 15:08:08 +0200

    Use date-fns without fp extensions

--- src/components/resume/date.ts
+++ src/components/resume/date.ts
@@ -1,12 +1,12 @@
 import { Result } from 'true-myth';
-import { format, parseISO } from 'date-fns/fp';
+import { format, parseISO } from 'date-fns';
 
 export function formatSinceDate(since: string, onlyYear: boolean): Result<string, string> {
     try {
         const formatString = onlyYear ? 'yyyy' : 'MMMM yyyy';
-        const formatToString = format(formatString);
+        const formatToString = format(parseISO(since), formatString);
 
-        return Result.ok(formatToString(parseISO(since)));
+        return Result.ok(formatToString);
     } catch {
         return Result.err(`Since "${since}" is not a valid date`);
     }
