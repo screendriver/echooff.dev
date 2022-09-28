55ff8dd6c599ffc43155dde56968e3f10cdc5b0f
Christian Rackerseder <git@echooff.de>
2022-09-28 09:13:18 +0200

    Create resume in Astro and Svelte components

---
+++ src/components/resume/data-schema.ts
@@ -0,0 +1,48 @@
+import { Result } from 'true-myth';
+import { z, ZodError } from 'zod';
+
+const filledStringSchema = z.string().min(1);
+
+const resumesSchema = z
+    .array(
+        z
+            .object({
+                since: filledStringSchema,
+                showOnlyYear: z.boolean(),
+                industry: filledStringSchema,
+                jobTitle: filledStringSchema,
+                jobDescription: filledStringSchema,
+                company: z
+                    .object({
+                        name: filledStringSchema,
+                        url: z.string().url(),
+                    })
+                    .strict(),
+            })
+            .strict(),
+    )
+    .nonempty();
+
+export type ResumesData = z.infer<typeof resumesSchema>;
+
+function formatParseError(error: ZodError): string {
+    return error.issues
+        .map((issue) => {
+            if (issue.path.length === 0) {
+                return issue.message;
+            }
+
+            return `${issue.path.join('.')}: ${issue.message}`;
+        })
+        .join('\n');
+}
+
+export function parseResumeData(resumeData: unknown): Result<ResumesData, string> {
+    const parseResult = resumesSchema.safeParse(resumeData);
+
+    if (parseResult.success) {
+        return Result.ok(parseResult.data);
+    }
+
+    return Result.err(formatParseError(parseResult.error));
+}
---
+++ src/components/resume/date.ts
@@ -0,0 +1,13 @@
+import { Result } from 'true-myth';
+import { format, parseISO } from 'date-fns/fp';
+
+export function formatSinceDate(since: string, onlyYear: boolean): Result<string, string> {
+    try {
+        const formatString = onlyYear ? 'yyyy' : 'MMMM yyyy';
+        const formatToString = format(formatString);
+
+        return Result.ok(formatToString(parseISO(since)));
+    } catch {
+        return Result.err(`Since "${since}" is not a valid date`);
+    }
+}
