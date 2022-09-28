de0bccc75496c64c8e01321f3a0a7b1c12268442
Christian Rackerseder <git@echooff.de>
2022-09-28 16:55:35 +0200

    Scroll in end-to-end tests because of Astro islands
    They will be only visible when they are scrolled into view.

--- test/e2e/contact-form.test.ts
+++ test/e2e/contact-form.test.ts
@@ -2,6 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('sends the contact form and shows a "Thank you" message', async ({ page }) => {
     await page.goto('/');
+    await page.evaluate(() => {
+        document.querySelector('section:nth-of-type(4)')?.scrollIntoView();
+    });
+    await page.evaluate(() => {
+        document.querySelector('section:nth-of-type(6)')?.scrollIntoView();
+    });
+
+    await page.waitForTimeout(3_000);
+
     await page.waitForSelector('text="Send Message"');
     await page.locator('[placeholder="Name"]').fill('My name');
     await page.locator('[placeholder="Email"]').fill('foo@example.com');
