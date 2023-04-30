428ba01e5bdcfa658e2e63d316780119cb49dc0d
Christian Rackerseder <git@echooff.de>
2023-04-30 14:34:07 +0200

    Create /github-statistics Astro server endpoint

--- test/unit/github-statistics/environment-variables.test.ts
+++ test/unit/github-statistics/environment-variables.test.ts
@@ -5,7 +5,7 @@ import {
 	gitHubApiTokenSchema,
 	gitHubBaseUrlSchema,
 	gitHubLoginSchema
-} from "../../../../../src/netlify/functions/github-statistics/environment-variables";
+} from "../../../src/github-statistics/environment-variables";
 
 test("gitHubBaseUrlSchema does not allow booleans", () => {
 	assert.throws(
