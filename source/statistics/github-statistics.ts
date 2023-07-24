d71b22b47cbf5f2facb41db1a560385ff13a50f1
Christian Rackerseder <git@echooff.de>
2023-07-24 09:39:53 +0200

    Fetch GitHub statistics outside of store context

---
+++ source/statistics/github-statistics.ts
@@ -0,0 +1,37 @@
+import { Result } from "true-myth";
+import is from "@sindresorhus/is";
+import type KyInterface from "ky";
+import type { GitHubStatistics } from "../github-statistics/github-statistics-schema";
+import { gitHubStatisticsSchema } from "../github-statistics/github-statistics-schema";
+
+interface FetchGitHubStatisticsDependencies {
+	readonly ky: typeof KyInterface;
+}
+
+export async function fetchGitHubStatistics(
+	dependencies: FetchGitHubStatisticsDependencies,
+): Promise<Result<unknown, string>> {
+	try {
+		const { ky } = dependencies;
+
+		const gitHubStatistics = await ky("/api/github-statistics").json();
+
+		return Result.ok(gitHubStatistics);
+	} catch (error: unknown) {
+		if (is.error(error)) {
+			return Result.err(error.message);
+		}
+
+		return Result.err("An unknown error occurred while fetching GitHub statistics");
+	}
+}
+
+export function parseGitHubStatistics(gitHubStatistics: unknown): Result<GitHubStatistics, string> {
+	const parsedGitHubStatistics = gitHubStatisticsSchema.safeParse(gitHubStatistics);
+
+	if (parsedGitHubStatistics.success) {
+		return Result.ok(parsedGitHubStatistics.data);
+	}
+
+	return Result.err(parsedGitHubStatistics.error.message);
+}
