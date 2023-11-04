4639825a117d16dcaa59cab3cc84a3c634560d8e
Christian Rackerseder <git@echooff.de>
2023-11-04 15:28:26 +0100

    Remove Vercel configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
 	srcDir: "source",
 	output: "static",
-	adapter: vercel({
-		imageService: true,
-		imagesConfig: {
-			sizes: [640, 768, 1024, 1280, 1920],
-			formats: ["image/avif", "image/webp"],
-			domains: [],
-		},
-	}),
 	integrations: [tailwind()],
 	image: {
 		service: sharpImageService(),
