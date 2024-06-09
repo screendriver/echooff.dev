0e14f86fe3eb0a3bcacce543ae827407bbacba61
Christian Rackerseder <git@echooff.de>
2024-06-09 19:21:09 +0200

    Switch to hybrid mode in Astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig, sharpImageService } from "astro/config";
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
 	srcDir: "source",
-	output: "static",
-	outDir: "./target/dist",
-	integrations: [tailwind()],
+	output: "hybrid",
+	outDir: "./target",
+	adapter: node({
+		mode: "standalone",
+	}),
 	image: {
 		service: sharpImageService(),
 	},
@@ -13,6 +16,9 @@ export default defineConfig({
 		shikiConfig: {
 			theme: "dracula",
 		},
+	},
+	server: {
+		port: 4321,
 	},
 	site: "https://www.echooff.dev",
 });
