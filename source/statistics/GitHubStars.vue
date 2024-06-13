39715a10d33d40da3b1109761ecf3b9037799ab0
Christian Rackerseder <git@echooff.de>
2024-06-13 09:07:22 +0200

    Render loading indicator while loading GitHub statistics

--- source/statistics/GitHubStars.vue
+++ source/statistics/GitHubStars.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
+import type { Maybe } from "true-myth";
+import type { GitHubStatistics } from "../github-statistics/github-statistics-schema";
 import Figure from "./Figure.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 import Cite from "./Cite.vue";
 
 interface Properties {
-	readonly starredRepositoriesTotalCount: number;
+	readonly isFetching: boolean;
+	readonly gitHubStatistics: Maybe<GitHubStatistics>;
 }
 
-const { starredRepositoriesTotalCount } = defineProps<Properties>();
+const { isFetching, gitHubStatistics } = defineProps<Properties>();
 </script>
 
 <template>
 	<Figure description="GitHub Stars">
-		<Cite ariaLabel="GitHub Stars">
-			{{ starredRepositoriesTotalCount }}
+		<LoadingSpinner v-if="isFetching" />
+		<Cite v-if="!isFetching" ariaLabel="GitHub Stars">
+			{{ gitHubStatistics.get("user").get("starredRepositories").get("totalCount").unwrapOr(0) }}
 		</Cite>
 	</Figure>
 </template>
