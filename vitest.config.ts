46b64b15abea5ae288ea73c999da6f3817c3e30a
Christian Rackerseder <git@echooff.de>
2025-07-24 13:51:57 +0200

    Remove code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,4 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
 
-export default getViteConfig({
-	test: {
-		coverage: {
-			reportsDirectory: "./target/coverage",
-			exclude: ["astro.config.ts", "postcss.config.cjs", "tailwind.config.cjs", "target/"],
-		},
-	},
-});
+export default getViteConfig({});
